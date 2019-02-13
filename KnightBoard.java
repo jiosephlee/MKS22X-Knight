78d599cf904367875d3f40bdae3462cfbf598199
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-02-13 10:16:06 -0500

    finished writing all functions, haven't tested

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,9 +1,18 @@
 public class KnightBoard{
 
     private int[][] board;
+    private int area;
+    private final int[][] moves;
 
     public KnightBoard(int startingRows,int startingCols){
         board = new int[startingRows][startingCols];
+        area = startingRows * startingCols;
+        moves = new int[][] {
+                {2,1}, {2,-1},
+                {-2,1}, {-2,-1},
+                {1,2}, {-1,-2},
+                {1,2}, {-1,-2}
+                };
     }
 
     public String toString(){
@@ -22,14 +31,42 @@ public class KnightBoard{
     }
 
     public boolean solve(int startingRow, int startingCol){
-        return solveH(startingRow, startingCol, 0);
-    }
-
-    public int countSolutions(int startingRow, int startingCol){
-
+        return solveH(startingRow, startingCol, 1);
     }
 
     private boolean solveH(int row ,int col, int level){
+        if (level == area){
+            return true;
+        }
+        for (int[] i : moves){
+            if (board[row][col] == 0){
+                board[row][col] = level;
+                if(row + i[0] >= 0 || row + i[0] < board.length || col + i[1] >= 0 || col + i[1] < board[0].length){
+                    return solveH(row + i[0], col + i[1], level + 1);
+            }
+            board[row + i[0]][col + i[1]] = 0;
+            }
+        }
+        return false;
+    }
 
+    public int countSolutions(int startingRow, int startingCol){
+        return countH(startingRow, startingCol, 1, 0);
+    }
+
+    private int countH(int row ,int col, int level, int count){
+        if (level == area){
+            count++;
+        }
+        for (int[] i : moves){
+            if (board[row][col] == 0){
+                board[row][col] = level;
+                if(row + i[0] >= 0 || row + i[0] < board.length || col + i[1] >= 0 || col + i[1] < board[0].length){
+                    count += countH(row + i[0], col + i[1], level + 1);
+            }
+            board[row + i[0]][col + i[1]] = 0;
+            }
+        }
+        return count;
     }
 }
