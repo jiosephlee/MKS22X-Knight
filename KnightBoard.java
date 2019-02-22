01f72b8d5b9c655440aebfbd746169413510ce3e
Joseph Lee <jlee01@stuy.edu>
2019-02-22 14:34:55 -0500

    fixing knight

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,9 +1,7 @@
 public class KnightBoard{
 
     private int[][] board;
-    private int area;
     private final int[][] moves;
-    private static int sum;
 
     public static void main(String[] args) {
         KnightBoard test = new KnightBoard(5,3);
@@ -16,7 +14,6 @@ public class KnightBoard{
     }
     public KnightBoard(int startingRows,int startingCols){
         board = new int[startingRows][startingCols];
-        area = startingRows * startingCols;
         moves = new int[][] {
                 {2,1}, {2,-1},
                 {-2,1}, {-2,-1},
@@ -39,51 +36,65 @@ public class KnightBoard{
         }
         return output;
     }
+    public String debug(){
+        String output = "";
+        for(int i = 0;i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                output+= " " + board[i][j];
+            }
+            output += '\n';
+        }
+        return output;
+    }
 
     public boolean solve(int startingRow,int startingCol){
-        return solveH(startingRow,startingCol,0);
+        if(startingCol < 0 || startingRow < 0){
+            throw new IllegalArgumentException();
+        }
+        for (int[] row :board ) {
+            for (int num: row){
+                if(num != 0 ){
+                    throw new IllegalStateException();
+                }
+            }
+        }
+        return solveH(startingRow,startingCol,1);
     }
 
     private boolean solveH(int row ,int col, int level){
-        if (level == area){
+        System.out.println(this.debug());
+        if (level == board.length * board[0].length){
             return true;
         }
-        if (board[row][col] == 0){
-            board[row][col] = level;
-            for (int[] i : moves){
-                if(row + i[0] >= 0 && row + i[0] < board.length && col + i[1] >= 0 && col + i[1] < board[0].length){
-                    return solveH(row + i[0], col + i[1], level + 1);
-                }
-                if(row + i[0] >= 0 && row + i[0] < board.length && col + i[1] >= 0 && col + i[1] < board[0].length){
-                    board[row + i[0]][col + i[1]] = 0;
-                }
+        board[row][col] = level;
+        for (int[] i : moves){
+            System.out.println("yo");
+            if(row + i[0] >= 0 && row + i[0] < board.length && col + i[1] >= 0 && col + i[1] < board[0].length && board[row + i[0]][col + i[1]] == 0
+               && solveH(row + i[0], col + i[1], level + 1)){
+                return true;
             }
-            board[row][col] = 0;
         }
+        board[row][col] = 0;
         return false;
     }
 
     public int countSolutions(int startingRow,int startingCol){
-        return countH(startingRow, startingCol, 0, 0);
+        return countH(startingRow, startingCol, 0);
     }
 
-    private int countH(int row ,int col, int level, int output){
+    private int countH(int row ,int col, int level){
         int output = 0;
-        if (level == area){
+        if (level == board.length * board[0].length){
             return 1;
         }
-        if (board[row][col] == 0){
-            board[row][col] = level;
-            for (int[] i : moves){
-                if(row + i[0] >= 0 && row + i[0] < board.length && col + i[1] >= 0 && col + i[1] < board[0].length){
-                    output+= countH(row + i[0], col + i[1], level + 1);
-                }
-                if(row + i[0] >= 0 && row + i[0] < board.length && col + i[1] >= 0 && col + i[1] < board[0].length){
-                    board[row + i[0]][col + i[1]] = 0;
-                }
+        board[row][col] = level;
+        for (int[] i : moves){
+            if(row + i[0] >= 0 && row + i[0] < board.length && col + i[1] >= 0 && col + i[1] < board[0].length
+               && board[row + i[0]][col + i[1]] == 0){
+                output+= countH(row + i[0], col + i[1], level + 1);
             }
-            board[row][col] = 0;
         }
-        return 0;
+        board[row][col] = 0;
+        return output;
     }
 }
