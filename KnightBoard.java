46b92b6bcd43ddda55b23c88ddd8d9a980470b1d
Joseph Lee <jlee01@stuy.edu>
2019-02-22 22:43:49 -0500

    regular works perfectlygit add KnightBoard.java

--- KnightBoard.java
+++ KnightBoard.java
@@ -4,14 +4,15 @@ public class KnightBoard{
     private final int[][] moves;
 
     public static void main(String[] args) {
-        KnightBoard test = new KnightBoard(5,6);
+        KnightBoard test = new KnightBoard(4,4);
         System.out.println(test);
         System.out.println(test.solve(0,0));
         System.out.println(test);
-
+        test.clear();
         System.out.println(test.countSolutions(0,0));
         System.out.println(test);
     }
+
     public KnightBoard(int startingRows,int startingCols){
         board = new int[startingRows][startingCols];
         moves = new int[][] {
@@ -22,6 +23,13 @@ public class KnightBoard{
                 };
     }
 
+    public void clear(){
+        for(int i = 0;i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                board[i][j] = 0;
+            }
+        }
+    }
     public String toString(){
         String output = "";
         for(int i = 0;i < board.length; i++){
@@ -79,6 +87,16 @@ public class KnightBoard{
     }
 
     public int countSolutions(int startingRow,int startingCol){
+        if(startingCol < 0 || startingRow < 0){
+            throw new IllegalArgumentException();
+        }
+        for (int[] row :board ) {
+            for (int num: row){
+                if(num != 0 ){
+                    throw new IllegalStateException();
+                }
+            }
+        }
         return countH(startingRow, startingCol, 1);
     }
 
@@ -88,16 +106,16 @@ public class KnightBoard{
         board[row][col] = level;
         if (level == board.length * board[0].length){
             output++;
-            return output;
+            //System.out.println("here");
         }
         for (int[] i : moves){
-            //System.out.println("yo");
             if(row + i[0] >= 0 && row + i[0] < board.length && col + i[1] >= 0 && col + i[1] < board[0].length
                && board[row + i[0]][col + i[1]] == 0){
-                output+= countH(row + i[0], col + i[1], level + 1);
+                output += countH(row + i[0], col + i[1], level + 1);
             }
         }
         board[row][col] = 0;
+        //System.out.println(output);
         return output;
     }
 }
